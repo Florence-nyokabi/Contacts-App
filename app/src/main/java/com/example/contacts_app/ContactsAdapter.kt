766a2b3605e820750728fc16c10a267adc14ff0c
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-05-26 20:11:50 +0300

    Contacts App

--- app/src/main/java/com/example/contacts_app/ContactsAdapter.kt
+++ app/src/main/java/com/example/contacts_app/ContactsAdapter.kt
@@ -1,8 +1,15 @@
 package com.example.contacts_app
 
-class ContactAdapter(private val contacts: List<Contact>) : RecyclerView.Adapter<ContactViewHolder>() {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter(private val contacts: List<String>) : RecyclerView.Adapter<ContactViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_list_item, parent, false)
         return ContactViewHolder(view)
     }
 
@@ -13,5 +20,25 @@ class ContactAdapter(private val contacts: List<Contact>) : RecyclerView.Adapter
 
     override fun getItemCount(): Int {
         return contacts.size
+    }
+}
+class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val imageView = itemView.findViewById<ImageView>(R.id.imgAvator)
+    val textViewName = itemView.findViewById<TextView>(R.id.tvContactName)
+    val textViewPhoneNumber = itemView.findViewById<TextView>(R.id.tvPhoneNumber)
+    val textViewEmailAddress = itemView.findViewById<TextView>(R.id.tvEmailAddress)
+
+    fun bind(contact: Contact) {
+
+
+        // Set the contact name
+        textViewName.text = contact.name
+
+        // Set the contact phone number
+        textViewPhoneNumber.text = contact.phoneNumber
+
+        // Set the contact email address
+        textViewEmailAddress.text = contact.emailAddress
     }
 }
