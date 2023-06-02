2158e9947bc069c99126cdb00a56e875bc029b40
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-06-02 08:18:58 +0300

    Contacts App

--- app/src/main/java/com/example/contacts_app/MainActivity.kt
+++ app/src/main/java/com/example/contacts_app/MainActivity.kt
@@ -17,12 +17,33 @@ class MainActivity : AppCompatActivity() {
         showContacts()
     }
     fun showContacts(){
-        val contact1 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com",)
-        val contact2 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com",)
-        val contact3 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com",)
-        val contact4 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com",)
+        val contact1 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact2 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact3 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact4 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact5 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact6 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact7 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact8 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact9 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact10 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact11 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact12 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact13 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact14 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact15 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact16 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact17 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact18 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact19 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
 
-        val contactList= listOf(contact1,contact2,contact3,contact4)
+
+
+
+
+
+
+        val contactList= listOf(contact1,contact2,contact3,contact4, contact5, contact6, contact7, contact8, contact9, contact10, contact11)
         val contactsAdapt = ContactAdapter(contactList)
         binding.rvContactsList.layoutManager = LinearLayoutManager(this)
         binding.rvContactsList.adapter = contactsAdapt
