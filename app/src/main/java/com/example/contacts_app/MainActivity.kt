4f7fcf1769e359f364ea46f85480c4f4486b0526
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-06-02 08:27:54 +0300

    Contacts App

--- app/src/main/java/com/example/contacts_app/MainActivity.kt
+++ app/src/main/java/com/example/contacts_app/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         val contact17 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
         val contact18 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
         val contact19 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
+        val contact20 = ContactsDataClass(" ", "Flo", "07122232232", "flo@gmal.com")
 
 
 
@@ -43,7 +44,9 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        val contactList= listOf(contact1,contact2,contact3,contact4, contact5, contact6, contact7, contact8, contact9, contact10, contact11)
+
+
+        val contactList= listOf(contact1,contact2,contact3,contact4, contact5, contact6, contact7, contact8, contact9, contact10, contact11, contact12, contact13, contact14, contact15, contact16, contact17, contact18, contact19, contact20)
         val contactsAdapt = ContactAdapter(contactList)
         binding.rvContactsList.layoutManager = LinearLayoutManager(this)
         binding.rvContactsList.adapter = contactsAdapt
